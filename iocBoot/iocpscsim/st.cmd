df7da902d7bb9522c58fb6e0d2a6d51be261dce2
Michael Davidsaver <mdavidsaver@gmail.com>
2020-12-07 09:53:26 -0800

    add optional autosave

--- iocBoot/iocpscsim/st.cmd
+++ iocBoot/iocpscsim/st.cmd
@@ -15,4 +15,14 @@ var(PSCDebug, 2)
 createPSC("test", "localhost", 8765, 1)
 setPSCSendBlockSize("test", 1, 20)
 
+#set_savefile_path("$(PWD)/as","/save/")
+#set_requestfile_path("$(PWD)/as","/req/")
+#system "install -m 744 -d $(PWD)/as/save"
+#system "install -m 744 -d $(PWD)/as/req"
+
+#set_pass0_restoreFile("settings.sav")
+
 iocInit()
+
+#makeAutosaveFileFromDbInfo("$(PWD)/as/req/settings.req", "autosaveFields_pass0")
+#create_monitor_set("settings.req", 10 , "")
